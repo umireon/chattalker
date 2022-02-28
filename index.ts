6b25ddcd8119209876748b50a39bd3a9a011f942
Kaito Udagawa <umireon@gmail.com>
2022-02-28 23:37:35 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,30 @@
 import { http } from '@google-cloud/functions-framework'
 import { TextToSpeechClient } from '@google-cloud/text-to-speech'
-import { v2 } from '@google-cloud/translate'
+import { TranslationServiceClient } from '@google-cloud/translate'
 import { initializeApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import { encode } from '@msgpack/msgpack'
 
 const client = new TextToSpeechClient()
-const translate = new v2.Translate()
+const translationClient = new TranslationServiceClient();
 const app = initializeApp()
 const auth = getAuth(app)
+
+const detectLanguage = async (content: string) => {
+  const [response] = await translationClient.detectLanguage({ content })
+  const { languages } = response
+  if (!languages) return 'und'
+  const [ { languageCode } ] = languages
+  return languageCode || 'und'
+}
+
+const getVoice = (languageCode: string) => {
+  if (languageCode === 'und') {
+    return { languageCode: 'en' }
+  } else {
+    return { languageCode }
+  }
+}
 
 http('helloHttp', async (req, res) => {
   res.set('Access-Control-Allow-Origin', 'https://umireon-twitch-speech-test1.web.app')
@@ -28,14 +44,12 @@ http('helloHttp', async (req, res) => {
 
   const text = req.query.text
   if (typeof text !== 'string') throw new Error('Error')
-  const [detections] = await translate.detect(text)
-  const [{ language }] = Array.isArray(detections) ? detections : [detections]
-  const languageCode = language === 'und' ? 'en-US' : language as string
+  const language = await detectLanguage(text)
 
   const [response] = await client.synthesizeSpeech({
     audioConfig: { audioEncoding: 'MP3' },
     input: { text },
-    voice: { languageCode }
+    voice: getVoice(language)
   })
   res.send(Buffer.from(encode({
     audioContent: response.audioContent,
