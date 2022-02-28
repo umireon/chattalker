c9261959832d7a752d891d13a0315668bfd66096
Kaito Udagawa <umireon@gmail.com>
2022-03-01 03:52:25 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ const getTwitchLogin = async (token: string) => {
 const generateVoiceTable = () => {
   const entries = []
   for (const element of document.querySelectorAll('select')) {
-    entries.push([element.id, element.value])
+    entries.push([element.id.replace(/^voice-/, ''), element.value])
   }
   return Object.fromEntries(entries)
 }
