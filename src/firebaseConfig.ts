f058f8e28fbe7b617c40f01d042111ef9acf4014
Kaito Udagawa <umireon@gmail.com>
2022-03-02 13:29:40 +0900

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAUej-VTeNFs6slwLE3M0J0NBz0BNdFODA',
-  appId: '1:110679332753:web:8cdf3d0736e80293aeec30',
-  authDomain: 'umireon-twitch-speech-test1.firebaseapp.com',
-  measurementId: 'G-Q8Y6670GB8',
-  messagingSenderId: '110679332753',
-  projectId: 'umireon-twitch-speech-test1',
-  storageBucket: 'umireon-twitch-speech-test1.appspot.com'
+  apiKey: 'AIzaSyDTH0B1DLTsmWZQRW8aJ7LWswpZ_tnWesE',
+  appId: '1:244926935062:web:dd2fe1ec4263d9f74d8537',
+  authDomain: 'chattalker.firebaseapp.com',
+  measurementId: 'G-T64BRZR9QC',
+  messagingSenderId: '244926935062',
+  projectId: 'chattalker',
+  storageBucket: 'chattalker.appspot.com'
 }
