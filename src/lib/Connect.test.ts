bf5696118862722e213841d07e2e6364e4ab859b
Kaito Udagawa <umireon@gmail.com>
2022-12-22 10:36:31 +0000

    lint

--- src/lib/Connect.test.ts
+++ src/lib/Connect.test.ts
@@ -1,8 +1,15 @@
+import { type AppContext } from "../../constants";
 import Connect from "./Connect.svelte";
+import { type Firestore } from "firebase/firestore";
+import { type User } from "firebase/auth";
 import { render } from "@testing-library/svelte";
 
 test("Connect snapshot", () => {
-  const props = { context: null, db: null, user: null };
+  const props = {
+    context: {} as AppContext,
+    db: {} as Firestore,
+    user: {} as User,
+  };
   const component = render(Connect, { props });
   expect(component.container).toMatchSnapshot();
 });
--- src/lib/GenerateUrl.test.ts
+++ src/lib/GenerateUrl.test.ts
@@ -1,8 +1,10 @@
+import { type Firestore } from "firebase/firestore";
 import GenerateUrl from "./GenerateUrl.svelte";
+import { type User } from "firebase/auth";
 import { render } from "@testing-library/svelte";
 
 test("GenerateUrl snapshot", () => {
-  const props = { db: null, user: null };
+  const props = { db: {} as Firestore, user: {} as User };
   const component = render(GenerateUrl, { props });
   expect(component.container).toMatchSnapshot();
 });
