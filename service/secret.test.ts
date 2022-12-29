eb49cc7e7b87ca57fcb38e7d2de9dee0dea34f29
Kaito Udagawa <umireon@gmail.com>
2022-12-29 16:02:35 +0900

    secret.test.ts

---
+++ service/secret.test.ts
@@ -0,0 +1,27 @@
+import {
+  DEFAULT_YOUTUBE_CLIENT_SECRET_VERSION,
+  YOUTUBE_CLIENT_SECRET_FIELD_NAME,
+  getYoutubeClientSecret,
+} from "./secret";
+import { describe, expect, it, vi } from "vitest";
+
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
+
+describe("getYoutubeClientSecret", () => {
+  it("accesses Secret Manager", async () => {
+    const accessSecretVersion = vi
+      .fn()
+      .mockResolvedValue([{ payload: { data: "data" } }]);
+    const client = {
+      accessSecretVersion,
+    } as unknown as InstanceType<typeof SecretManagerServiceClient>;
+    const actual = await getYoutubeClientSecret(
+      client,
+      { projectId: "projectId" }
+    );
+    expect(actual).toBe("data");
+    expect(accessSecretVersion.mock.calls[0][0]).toEqual({
+      name: `projects/projectId/secrets/${YOUTUBE_CLIENT_SECRET_FIELD_NAME}/versions/${DEFAULT_YOUTUBE_CLIENT_SECRET_VERSION}`,
+    });
+  });
+});
