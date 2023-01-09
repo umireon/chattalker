1a431f7ca5ace1979cdd6e8b4fed31957908f265
Kaito Udagawa <umireon@gmail.com>
2023-01-09 19:04:20 +0900

    Update oauth.test.ts

--- src/service/oauth.test.ts
+++ src/service/oauth.test.ts
@@ -1,21 +1,23 @@
-import { expect, test } from "vitest";
+import { describe, expect, it, test } from "vitest";
 
 import type { YoutubeOauthResponse } from "./oauth";
 import { validateYoutubeOauthResponse } from "./oauth";
 
-test("validateYoutubeOauthResponse agrees with valid YoutubeOauthResponse", () => {
-  const response: YoutubeOauthResponse = {
-    access_token: "",
-    expires_in: 0,
-    scope: "",
-    token_type: "Bearer",
-  };
-  expect(validateYoutubeOauthResponse(response)).toBeTruthy();
-});
+describe("validateYoutubeOauthResponse", () => {
+  it("agrees with valid YoutubeOauthResponse", () => {
+    const response: YoutubeOauthResponse = {
+      access_token: "",
+      expires_in: 0,
+      scope: "",
+      token_type: "Bearer",
+    };
+    expect(validateYoutubeOauthResponse(response)).toBeTruthy();
+  });
 
-test("validateYoutubeOauthResponse disagrees with invalid data", () => {
-  const response = {
-    error: "",
-  };
-  expect(validateYoutubeOauthResponse(response)).toBeFalsy();
+  it("agrees with valid YoutubeOauthResponse", () => {
+    const response = {
+      error: "",
+    };
+    expect(validateYoutubeOauthResponse(response)).toBeFalsy();
+  });
 });
