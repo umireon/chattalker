cc7e38c48575c6289b1525d06a224a0b44e829e3
Kaito Udagawa <umireon@gmail.com>
2023-01-09 18:58:52 +0900

    Player.test.ts

--- src/lib/Player.test.ts
+++ src/lib/Player.test.ts
@@ -1,15 +1,17 @@
-import { expect, test } from "vitest";
+import { describe, expect, it, test } from "vitest";
 
 import Player from "./Player.svelte";
 import { render } from "@testing-library/svelte";
 
-test("Player snapshot", () => {
-  const props = {
-    playerIsLoading: false,
-    playerLanguage: "",
-    playerSrc: "",
-    playerText: "",
-  };
-  const component = render(Player, { props });
-  expect(component.container).toMatchSnapshot();
-});
+describe("Player", () => {
+  it("matches snapshot", () => {
+    const props = {
+      playerIsLoading: false,
+      playerLanguage: "",
+      playerSrc: "",
+      playerText: "",
+    };
+    const component = render(Player, { props });
+    expect(component.container).toMatchSnapshot();
+  })
+})
