5d4c2ba5ce6347feacc08f4cb75552207d4dce42
Kaito Udagawa <umireon@gmail.com>
2023-01-09 18:53:11 +0900

    Connect.test.ts

--- src/lib/Connect.test.ts
+++ src/lib/Connect.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { describe, expect, it, test } from "vitest";
 
 import { type AppContext } from "../../constants";
 import Connect from "./Connect.svelte";
@@ -6,12 +6,14 @@ import { type Firestore } from "firebase/firestore";
 import { type User } from "firebase/auth";
 import { render } from "@testing-library/svelte";
 
-test("Connect snapshot", () => {
-  const props = {
-    context: {} as AppContext,
-    db: {} as Firestore,
-    user: {} as User,
-  };
-  const component = render(Connect, { props });
-  expect(component.container).toMatchSnapshot();
-});
+describe("Connect", () => {
+  it("matches snapshot", () => {
+    const props = {
+      context: {} as AppContext,
+      db: {} as Firestore,
+      user: {} as User,
+    };
+    const component = render(Connect, { props });
+    expect(component.container).toMatchSnapshot();
+  })
+})
