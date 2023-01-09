fb6d5079e95ff3b04b7711fd01ed8a7c4e11a190
Kaito Udagawa <umireon@gmail.com>
2023-01-09 19:00:00 +0900

    Voice.test.ts

--- src/lib/Voice.test.ts
+++ src/lib/Voice.test.ts
@@ -1,15 +1,17 @@
-import { expect, test } from "vitest";
+import { describe, expect, it, test } from "vitest";
 
 import Voice from "./Voice.svelte";
 import { render } from "@testing-library/svelte";
 
-test("Player snapshot", () => {
-  const props = {
-    playAudio: (_: string) => {},
-    voiceEn: "",
-    voiceJa: "",
-    voiceUnd: "",
-  };
-  const component = render(Voice, { props });
-  expect(component.container).toMatchSnapshot();
-});
+describe("Voice", () => {
+  it("matches snapshot", () => {
+    const props = {
+      playAudio: (_: string) => {},
+      voiceEn: "",
+      voiceJa: "",
+      voiceUnd: "",
+    };
+    const component = render(Voice, { props });
+    expect(component.container).toMatchSnapshot();
+  })
+})
